7113b31f5433dea2d2f80813c50a8274fb5d8790
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-12-31 12:43:33 +0300

    Initial commit

---
+++ src/main/java/Annotation/ParseTree/TestNERFrame.java
@@ -0,0 +1,8 @@
+package Annotation.ParseTree;
+
+public class TestNERFrame {
+
+    public static void main(String[] args){
+        new DataCollector.ParseTree.NER.TreeNERFrame();
+    }
+}
---
+++ src/main/java/Annotation/Sentence/TestSentenceNERFrame.java
@@ -0,0 +1,11 @@
+package Annotation.Sentence;
+
+import DataCollector.Sentence.NER.SentenceNERFrame;
+
+public class TestSentenceNERFrame {
+
+    public static void main(String[] args){
+        new SentenceNERFrame();
+    }
+
+}
